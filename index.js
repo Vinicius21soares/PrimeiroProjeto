dfea97f533ff6a54c7628dd22d9d86568de80319
Vinicius21soares <vini21soares@gmail>
2022-05-20 20:27:37 -0300

    Primeiro Projeto

---
+++ index.js
@@ -0,0 +1,18 @@
+const http = require('http');
+
+const hostname = '127.0.0.1';// Endereço do servidor ... está como local
+const port =  3000 // Porta do servvidor
+
+const serve = http.createServer((req, res) => { //Criando o servidor
+    res.statusCode = 200 // Status ok de resposta
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Ola Mundo Uba');
+
+});
+
+
+
+serve.listen(port, hostname, () => {
+
+   console.log(`Servidor rodando em http://${hostname}:${port}/`)
+})
