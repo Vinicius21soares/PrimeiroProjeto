341d1d9f0f46223e9c98957b7b5fc4bd2647d61e
Vinicius21soares <vini21soares@gmail.com>
2022-05-27 20:01:31 -0300

    Criando páginas com handlebars

--- index.js
+++ index.js
@@ -1,18 +1,32 @@
-const http = require('http');
+const express=require('express');// determinando agit utilização do express
+const app=express();
+const path=require ('path');//endereço de cada rota
+const router=express.Router();//trabalha com rotas
 
-const hostname = '127.0.0.1';// Endereço do servidor ... está como local
-const port =  3000 // Porta do servvidor
+const {engine} = require("express-handlebars");
 
-const serve = http.createServer((req, res) => { //Criando o servidor
-    res.statusCode = 200 // Status ok de resposta
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Atividade1');
+app.engine('handlebars', engine({
+    defaultLayout: 'main', 
+    runtimeOptions:{
+        allowProtoPropertiesByDefault: true,
+        allowProtoMethodsByDefaukt: true,
+    },
+}))
 
-});
-
-
-
-serve.listen(port, hostname, () => {
-
-   console.log(`Servidor rodando em http://${hostname}:${port}/`)
+app.set('view engine', 'handlebars');
+router.get('/', function(req,res){
+    res.render('index')
 })
+
+
+router.get('/', function(req,res){
+    res.render('/index');
+})
+router.get('/about', function(req,res){
+    res.render('/about');
+})
+
+app.use('/',router);
+app.use('/about',router);
+app.listen(process.env.port||3000);
+console.log("Servidor rodando");
